0737561c412cf0e85e3aa9ca22ba81b8244cd029
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2020-08-17 07:30:57 -0500

    build request and tests

--- Sources/HTTPEngine/HTTPEngine.swift
+++ Sources/HTTPEngine/HTTPEngine.swift
@@ -1,3 +1,33 @@
-struct HTTPEngine {
-    var text = "Hello, World!"
+import Foundation
+
+public typealias Header = [String: String]
+
+public struct HTTPEngine {
+    public init() {}
+
+    public func buildRequest(
+        method: HTTPMethod,
+        url: URL,
+        body: Data? = nil,
+        header: Header? = nil
+    ) -> URLRequest {
+
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue.uppercased()
+        request.httpBody = body
+        request.allHTTPHeaderFields = header
+
+        if request.allHTTPHeaderFields?["Content-Type"] == nil {
+            if [HTTPMethod.post, HTTPMethod.put, HTTPMethod.patch].contains(method) {
+                request.allHTTPHeaderFields?["Content-Type"] = "application/json"
+            }
+        }
+
+        if request.allHTTPHeaderFields?["Accept-Encoding"] == nil {
+            request.allHTTPHeaderFields?["Accept-Encoding"] = "gzip;q=1.0,compress;q=0.5"
+        }
+
+        return request
+    }
+    
 }
---
+++ Sources/HTTPEngine/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jacob Rakidzich on 8/17/20.
+//
+
+import Foundation
+
+public enum HTTPMethod: String, CaseIterable {
+    case post
+    case get
+    case patch
+    case delete
+    case put
+}
