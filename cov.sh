8728a7a54280576bfda526f33aa445485afdeed0
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2020-08-18 13:09:38 -0500

    trying another thing

--- cov.sh
+++ cov.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
-# brew ls --versions llvm
-# if [[ $? != 0 ]]; then
-#   brew install llvm
-# fi
+brew ls --versions llvm
 
-# BIN_PATH="$(swift build --show-bin-path)"
-# XCTEST_PATH="$(find ${BIN_PATH} -name '*.xctest')"
+if [[ $? != 0 ]]; then
+  brew install llvm
+fi
 
-# COV_BIN=$XCTEST_PATH
-# if [[ "$OSTYPE" == "darwin"* ]]; then
-#     f="$(basename $XCTEST_PATH .xctest)"
-#     COV_BIN="${COV_BIN}/Contents/MacOS/$f"
-# fi
+echo 'export PATH="/usr/local/opt/llvm/bin:$PATH"' >> ~/.zshrc
+BIN_PATH="$(swift build --show-bin-path)"
+XCTEST_PATH="$(find ${BIN_PATH} -name '*.xctest')"
 
-# llvm-cov report \
-#     "${COV_BIN}" \
-#     -instr-profile=.build/debug/codecov/default.profdata \
-#     -ignore-filename-regex=".build|Tests" \
-#     -use-color
+COV_BIN=$XCTEST_PATH
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    f="$(basename $XCTEST_PATH .xctest)"
+    COV_BIN="${COV_BIN}/Contents/MacOS/$f"
+fi
+
+llvm-cov report \
+    "${COV_BIN}" \
+    -instr-profile=.build/debug/codecov/default.profdata \
+    -ignore-filename-regex=".build|Tests" \
+    -use-color
 
 
 bash -c '<(curl -s https://codecov.io/bash)'
