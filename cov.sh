163addbb57f10a77885d6518fc0f7bd3c015ee4e
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2020-08-18 13:07:33 -0500

    trying a thing

--- cov.sh
+++ cov.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
-brew ls --versions llvm
-if [[ $? != 0 ]]; then
-  brew install llvm
-fi
+# brew ls --versions llvm
+# if [[ $? != 0 ]]; then
+#   brew install llvm
+# fi
 
-BIN_PATH="$(swift build --show-bin-path)"
-XCTEST_PATH="$(find ${BIN_PATH} -name '*.xctest')"
+# BIN_PATH="$(swift build --show-bin-path)"
+# XCTEST_PATH="$(find ${BIN_PATH} -name '*.xctest')"
 
-COV_BIN=$XCTEST_PATH
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    f="$(basename $XCTEST_PATH .xctest)"
-    COV_BIN="${COV_BIN}/Contents/MacOS/$f"
-fi
+# COV_BIN=$XCTEST_PATH
+# if [[ "$OSTYPE" == "darwin"* ]]; then
+#     f="$(basename $XCTEST_PATH .xctest)"
+#     COV_BIN="${COV_BIN}/Contents/MacOS/$f"
+# fi
 
-llvm-cov report \
-    "${COV_BIN}" \
-    -instr-profile=.build/debug/codecov/default.profdata \
-    -ignore-filename-regex=".build|Tests" \
-    -use-color
+# llvm-cov report \
+#     "${COV_BIN}" \
+#     -instr-profile=.build/debug/codecov/default.profdata \
+#     -ignore-filename-regex=".build|Tests" \
+#     -use-color
 
 
 bash -c '<(curl -s https://codecov.io/bash)'
